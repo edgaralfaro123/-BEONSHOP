58872c971eca747ea8d5e54018e2aaac8a5726fe
Edgar Alfaro <soporte2@email.cloudnets.co>
2021-02-24 13:55:00 -0500

    Agrego ultimos cambios

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,79 @@
-import React,{useEffect} from 'react';
-import {useSelector,useDispatch} from 'react-redux';
-import { obtenerPreguntasAction } from '../store/actions/questionsActions';
-const Home = () => {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(obtenerPreguntasAction())
-    }, [])
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { obtenerPreguntasAction } from "../store/actions/questionsActions";
+import Select from "react-select";
 
-    return ( 
-        <h1>hola gay</h1>
-     );
-}
- 
+const Home = () => {
+  const preguntas = useSelector((state) => state.questions.preguntas);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(obtenerPreguntasAction());
+  }, []);
+
+  const [datos, setdatos] = useState({});
+
+  const leerDatos = (value, name) => {
+    console.log("escribiendo ", value, " en ", name);
+
+    setdatos({ ...datos, [name]: value });
+  };
+
+  const validarEstados = () => {
+    first_name;
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div class="form-group">
+            {preguntas.length > 0 &&
+              preguntas.map((pregunta, key) => (
+                <>
+                  <h2>{pregunta.title}</h2>
+                  <div key={key}>
+                    {pregunta.fields.map((input) =>
+                      input.type === "text" ? (
+                        <>
+                          <label>{input.label}</label>
+                          <input
+                            name={`${input.name}`}
+                            type="text"
+                            className="form-control"
+                            placeholder={input.label}
+                            maxLength={30}
+                            onChange={(e) =>
+                              leerDatos(e.target.value, input.name)
+                            }
+                          />
+                        </>
+                      ) : (
+                        input.type === "dropdown" && (
+                          <>
+                            <label>{input.label}</label>
+                            <Select
+                              defaultValue={input.options[0]}
+                              name={input.name}
+                              options={input.options} // set list of the data
+                              onChange={(e) => leerDatos(e.value, input.name)}
+                            />
+                          </>
+                        )
+                      )
+                    )}
+                  </div>
+                  <hr></hr>
+                </>
+              ))}
+          </div>
+        </div>
+        <div className="col-6 pt-3 ">
+          <button className="btn btn-primary">Mostrar dom</button>
+          <code></code>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
--- src/store/actions/questionsActions.js
+++ src/store/actions/questionsActions.js
@@ -1,62 +1,71 @@
 import {
-    AGREGAR_PREGUNTAS,
-    AGREGAR_PREGUNTAS_ERROR,
-    AGREGAR_PREGUNTAS_EXITO,
-    OBETENER_PREGUNTAS_EXITO,
-    COMENZAR_DESCARGA_PREGUNTAS
-  } from "../types";
-  import preguntaAxios from "../config/axios";
-  
-  //crear nuevas preguntas
-  export const crearNuevaPregunta = (pregunta) => async (dispatch) => {
-    dispatch(agregarPregunta());
-  
-    try {
-      //insertar en la api
-      await preguntaAxios.post("/questions", pregunta);
-      //actualizar state
-      dispatch(agregarPreguntaExito(pregunta));
-    } catch (error) {
-      console.log(error);
-      dispatch(agregarPreguntaError(true));
-    }
-  };
-  
-  const agregarPregunta = () => ({
-    type: AGREGAR_PREGUNTAS,
-    payload: true,
-  });
-  
-  const agregarPreguntaExito = (estado) => ({
-    type: AGREGAR_PREGUNTAS_EXITO,
-    payload: estado,
-  });
+  AGREGAR_PREGUNTAS,
+  AGREGAR_PREGUNTAS_ERROR,
+  AGREGAR_PREGUNTAS_EXITO,
+  OBETENER_PREGUNTAS_EXITO,
+  COMENZAR_DESCARGA_PREGUNTAS,
+} from "../types";
+import preguntaAxios from "../config/axios";
 
-  const agregarPreguntaError = (estado) => ({
-    type: AGREGAR_PREGUNTAS_ERROR,
-    payload: estado,
-  });
+//crear nuevas preguntas
+export const crearNuevaPregunta = (pregunta) => async (dispatch) => {
+  dispatch(agregarPregunta());
 
-export const obtenerPreguntasAction = () => async (dispatch) => {
-    dispatch( {
-        type: COMENZAR_DESCARGA_PREGUNTAS,
-        payload: true
-    })
+  try {
+    //insertar en la api
+    await preguntaAxios.post("/questions", pregunta);
+    //actualizar state
+    dispatch(agregarPreguntaExito(pregunta));
+  } catch (error) {
+    console.log(error);
+    dispatch(agregarPreguntaError(true));
+  }
+};
 
-    try {
-        //insertar en la api
-        const datos = await preguntaAxios.get("/questions");
-        console.log("datos",datos);
-        dispatch(obtenerPreguntasExito(datos.data));
-      } catch (error) {
-        console.log(error);
-      }
-}
-
-const obtenerPreguntasExito = (datos) => ({
-    type: OBETENER_PREGUNTAS_EXITO,
-    payload: datos,
+const agregarPregunta = () => ({
+  type: AGREGAR_PREGUNTAS,
+  payload: true,
 });
 
-  
+const agregarPreguntaExito = (estado) => ({
+  type: AGREGAR_PREGUNTAS_EXITO,
+  payload: estado,
+});
 
+const agregarPreguntaError = (estado) => ({
+  type: AGREGAR_PREGUNTAS_ERROR,
+  payload: estado,
+});
+
+export const obtenerPreguntasAction = () => async (dispatch) => {
+  dispatch({
+    type: COMENZAR_DESCARGA_PREGUNTAS,
+    payload: true,
+  });
+
+  try {
+    //insertar en la api
+    const datos = await preguntaAxios.get("/questions");
+
+    datos.data.map((question) => {
+      let arr = [];
+      question.fields.map((field) => {
+        if (field.options) {
+          field.options.map((option) => {
+            arr.push({ label: option, value: option });
+          });
+          field.options = arr;
+        }
+      });
+    });
+    //console.log("datos", datos);
+    dispatch(obtenerPreguntasExito(datos.data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const obtenerPreguntasExito = (datos) => ({
+  type: OBETENER_PREGUNTAS_EXITO,
+  payload: datos,
+});
