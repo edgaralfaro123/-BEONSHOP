b1bbaa8bde95caf8209b3e4a992e14d9624589a3
Camilo Pérez <soporte1@email.cloudnets.co>
2021-02-25 13:43:35 -0500

    chore: mostrando y ocultando info

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -11,15 +11,12 @@ const Home = () => {
   }, []);
 
   const [datos, setdatos] = useState({});
+  const [mostrarDatos, setMostrarDatos] = useState(false)
 
   const leerDatos = (value, name) => {
     console.log("escribiendo ", value, " en ", name);
 
     setdatos({ ...datos, [name]: value });
-  };
-
-  const validarEstados = () => {
-    //first_name;
   };
 
   return (
@@ -48,28 +45,45 @@ const Home = () => {
                           />
                         </>
                       ) : (
-                        input.type === "dropdown" && (
-                          <>
-                            <label>{input.label}</label>
-                            <Select
-                              defaultValue={input.options[0]}
-                              name={input.name}
-                              options={input.options} // set list of the data
-                              onChange={(e) => leerDatos(e.value, input.name)}
-                            />
-                          </>
+                          input.type === "dropdown" && (
+                            <>
+                              <label>{input.label}</label>
+                              <Select
+                                defaultValue={input.options[0]}
+                                name={input.name}
+                                options={input.options} // set list of the data
+                                onChange={(e) => leerDatos(e.value, input.name)}
+                              />
+                            </>
+                          )
                         )
-                      )
                     )}
                   </div>
                   <hr></hr>
                 </>
               ))}
           </div>
+          <button className="btn btn-primary">Guardar info</button>
         </div>
         <div className="col-6 pt-3 ">
-          <button className="btn btn-primary">Mostrar dom</button>
-          <code></code>
+          <button onClick={() => setMostrarDatos(!mostrarDatos)} className="btn btn-primary">{mostrarDatos ? 'Ocultar' : 'Mostrar'} dom</button>
+          <code>
+            {JSON.stringify(datos)}
+          </code>
+          <div>
+            {mostrarDatos && (
+              <ul className="list-group">
+                <li className="list-group-item"><strong>firstName:</strong> {datos.first_name}</li>
+                <li className="list-group-item"> <strong>lastName:</strong> {datos.last_name}</li>
+                <li className="list-group-item"><strong>country:</strong> {datos.country}</li>
+                <li className="list-group-item"> <strong>email:</strong> {datos.fernandooroperasdasdasdsa}</li>
+                <li className="list-group-item"><strong> phoneNumber:</strong> {datos.phone_number}</li>
+                <li className="list-group-item"> <strong> postCode:</strong> {datos.post_code}</li>
+                <li className="list-group-item"><strong>streetAddress:</strong> {datos.street_address}</li>
+              </ul>
+            )}
+
+          </div>
         </div>
       </div>
     </div>
